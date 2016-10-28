a9ba1b9eb54464f2d7a05bf2671ece800c82e5cc
Avital Oliver <avital@theWE.net>
2016-10-28 11:23:05 -0700

    Make prediction work in separate thread.
    Also adapt to new dimensions for training

--- predict/predict.py
+++ predict/predict.py
@@ -17,6 +17,9 @@ import librosa
 import subprocess
 from keras.models import model_from_json
 import multiprocessing
+import frequency_stats
+import tensorflow as tf
+from keras import backend as K
 
 audio = pyaudio.PyAudio()
 
@@ -31,6 +34,11 @@ def load_model(weights_file):
     return model
 
 model = load_model('weights.hdf5')
+
+# Store TensorFlow graph. Needed to run inference in a different
+# thread.  See
+# https://www.tensorflow.org/versions/r0.11/api_docs/python/framework.html#get_default_graph
+graph = tf.get_default_graph()
 
 class AudioRecording():
     def start(self):
@@ -51,30 +59,45 @@ class AudioRecording():
                 frames.pop(0)
                 if num_frames % 12 == 0:
                     frames_array = b''.join(frames)
-                    test_segment(numpy.fromstring(frames_array, dtype=numpy.int16))
+                    Thread(
+                        target=test_segment,
+                        args=(numpy.fromstring(frames_array, dtype=numpy.int16),)
+                    ).start()
 
 decayed_prediction = 0.5
+
+def center_unit(spectrogram):
+    '''
+    Apply a linear transformation on the spectrogram that
+    emperically gets each entry to have a distribution with
+    mean 0 and standard deviation 1
+    '''
+
+    return ((spectrogram.T - frequency_stats.means[0:185]) / frequency_stats.stds[0:185]).T
 
 def normalize_spectrogram(spectrogram):
     spectrogram2 = spectrogram + 0.00001
     return spectrogram2 / numpy.sum(spectrogram2, 0)
 
 def test_segment(segment):
-    abs_spectrogram = numpy.absolute(librosa.stft(segment, n_fft=512))
-    abs_spectrogram = abs_spectrogram[:, 0:173]
-    normalized_spectrogram = normalize_spectrogram(abs_spectrogram)
-    sample_segment_spectrogram = numpy.expand_dims(normalized_spectrogram, axis=0)
-    sample_segment_spectrogram = numpy.expand_dims(sample_segment_spectrogram, axis=0)
-    prediction = model.predict(sample_segment_spectrogram)[0][0]
+    global graph
 
-    global decayed_prediction
-    decayed_prediction = decayed_prediction * 0.6 + prediction * 0.4
+    with graph.as_default():
+        abs_spectrogram = numpy.absolute(librosa.stft(segment, n_fft=512))
+        abs_spectrogram = abs_spectrogram[0:185, 0:173]
+        normalized_spectrogram = center_unit(normalize_spectrogram(abs_spectrogram))
+        sample_segment_spectrogram = numpy.expand_dims(normalized_spectrogram, axis=0)
+        sample_segment_spectrogram = numpy.expand_dims(sample_segment_spectrogram, axis=0)
+        prediction = model.predict(sample_segment_spectrogram)[0][0]
 
-    predictions_str = [' '] * 70
-    num_squares = int(round(0.999 * prediction * len(predictions_str)))
-    predictions_str[0:num_squares] = [unichr(9608)] * num_squares; # http://www.fileformat.info/info/unicode/char/2588/index.htm
+        global decayed_prediction
+        decayed_prediction = decayed_prediction * 0.6 + prediction * 0.4
 
-    print u'Not Speech [{0}] Speech ({1}%)'.format(u''.join(predictions_str), int(round(prediction * 100)))
+        predictions_str = [' '] * 70
+        num_squares = int(round(0.999 * prediction * len(predictions_str)))
+        predictions_str[0:num_squares] = [unichr(9608)] * num_squares; # http://www.fileformat.info/info/unicode/char/2588/index.htm
+
+        print u'Not Speech [{0}] Speech ({1}%)'.format(u''.join(predictions_str), int(round(prediction * 100)))
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
